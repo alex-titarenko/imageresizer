c468ae45172295a9f4607007dadf2acb0a6404b1
Alex Titarenko <alx.titarenko@gmail.com>
2020-05-17 12:26:51 -0700

    fix configuration

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,8 +1,13 @@
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TAlex.ImageProxy;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
 
-[assembly: FunctionsStartup(typeof(TAlex.ImageProxy.Startup))]
+[assembly: FunctionsStartup(typeof(Startup))]
 
 namespace TAlex.ImageProxy
 {
@@ -12,13 +17,40 @@ namespace TAlex.ImageProxy
         {
             builder.Services
                 .AddOptions<ProxySettings>()
-                .Configure<IConfiguration>((settings, configuration) =>
-                {
-                    configuration.GetSection("ProxySettings").Bind(settings);
-                    //configuration.Bind("ProxySettings", settings);
-                });
+                .Configure<IConfiguration>((settings, configuration) => configuration.GetSection("ProxySettings").Bind(settings));
+
+            FixConfiguration(builder.Services);
 
             builder.Services.AddSingleton<IImageProxyService, ImageProxyService>();
+        }
+
+        public static void FixConfiguration(IServiceCollection services)
+        {
+            var providers = new List<IConfigurationProvider>();
+
+            foreach (var descriptor in services.Where(descriptor => descriptor.ServiceType == typeof(IConfiguration)).ToList())
+            {
+                var existingConfiguration = descriptor.ImplementationInstance as IConfigurationRoot;
+                if (existingConfiguration is null)
+                {
+                    continue;
+                }
+                providers.AddRange(existingConfiguration.Providers);
+                services.Remove(descriptor);
+            }
+
+            var executioncontextoptions = services.BuildServiceProvider()
+                .GetService<IOptions<ExecutionContextOptions>>().Value;
+            var currentDirectory = executioncontextoptions.AppDirectory;
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(currentDirectory)
+                .AddJsonFile("local.settings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            providers.AddRange(config.Build().Providers);
+
+            services.AddSingleton<IConfiguration>(new ConfigurationRoot(providers));
         }
     }
 }
