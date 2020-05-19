719ab3eb1a22700fd2f5c0ca975ae337159555cb
Alex Titarenko <alx.titarenko@gmail.com>
2020-05-18 21:16:04 -0700

    keep original aspect ration

--- src/ImageResizerService.cs
+++ src/ImageResizerService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -68,7 +67,12 @@ namespace TAlex.ImageProxy
             var resultStream = new MemoryStream();
             using (var image = Image.Load(originalStream))
             {
-                image.Mutate(x => x.Resize(size.Width, size.Height));
+                var resizeOptions = new ResizeOptions
+                {
+                    Size = new Size(size.Width, size.Height),
+                    Mode = ResizeMode.Max
+                };
+                image.Mutate(x => x.Resize(resizeOptions));
                 image.SaveAsPng(resultStream);
                 resultStream.Position = 0;
             }
@@ -78,11 +82,6 @@ namespace TAlex.ImageProxy
                 throw new NotImplementedException();
             }
             return resultStream;
-        }
-
-        private static Stream OpenImageStream(string fileName)
-        {
-            return new FileStream(fileName, FileMode.Open, FileAccess.Read);
         }
 
         private ImageSize StringToImageSize(string value)
@@ -103,26 +102,6 @@ namespace TAlex.ImageProxy
                 targetUrl = Encoding.UTF8.GetString(Convert.FromBase64String(url.Substring(7)));
             }
             return new Uri(targetUrl.StartsWith(Uri.UriSchemeHttp) ? targetUrl : (Uri.UriSchemeHttp + Uri.SchemeDelimiter + targetUrl));
-        }
-
-        private static string GetPathName(Uri uri, string? defaultExtension = null)
-        {
-            char dirSeparator = Path.DirectorySeparatorChar;
-
-            // construct path in the hard disk
-            string strLocalPath = uri.LocalPath.Replace('/', dirSeparator);
-
-            // check if the path ends with / to can crate the file on the HD
-            if (strLocalPath.EndsWith(dirSeparator.ToString()))
-            {
-                string fileName = string.IsNullOrEmpty(uri.Query) ? "default" : uri.Query.GetHashCode().ToString();
-                strLocalPath =
-                    (string.IsNullOrEmpty(defaultExtension)) ?
-                    string.Format("{0}{1}", strLocalPath, fileName)
-                    : string.Format("{0}{1}.{2}", strLocalPath, fileName, defaultExtension);
-            }
-
-            return String.Format("{0}{1}{2}", dirSeparator, uri.Host, strLocalPath.Replace("%20", " "));
         }
 
         private void SaveOriginalFileToStorage(Stream imageStream, Uri uri)
